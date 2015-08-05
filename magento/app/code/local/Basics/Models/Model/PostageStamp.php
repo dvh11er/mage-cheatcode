5463abf8ba43a4bb28ed6900189669549a6d7504
David Haller <dvh11er@gmail.com>
2015-08-05 09:33:44 -0400

    rename class

---
+++ magento/app/code/local/Basics/Models/Model/PostageStamp.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * To create a new instance of this model class, call:
+ *  - Mage::getModel('basics_models/postageStamp')
+ *
+ * The string 'basics_models' must match the name of the node in config.xml
+ * with the path: config/global/models/basics_models.
+ *
+ * The string 'postageStamp' must match the name of this file.
+ * The first letter will automatically be converted to upper case.
+ */
+class Basics_ModelResources_Model_PostageStamp
+{
+    public function myAction()
+    {
+        echo 'In ' . __METHOD__;
+    }
+}
